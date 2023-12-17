f326af33d50c2e6e0685253656c890ad14df9aaa
56mery <143891955+56mery@users.noreply.github.com>
2023-12-18 01:18:45 +0300

    lastfortoday dizi.c

--- c programing/dizi.c
+++ c programing/dizi.c
@@ -34,3 +34,39 @@ int main() {
 
 //abi yaptım :") (şimdi de pull yapıcam )
 //yaappptttııımmm şükür 
+
+//matrix oluşturma ve determinant
+
+int main() {
+    int det=0;
+   int matrix[2][2];
+   int i,j;
+   for(i=0; i<2; i++){
+     for(j=0; j<2; j++){
+         printf("matrix[%d][%d] için değer giriniz:" ,i,j);
+         scanf("%d" ,&matrix[i][j]);
+     }
+   }
+   
+   for(i=0; i<2; i++){
+       for(j=0; j<2; j++){
+           printf("matrix[%d][%d]=%d\n" ,i,j,matrix[i][j]);
+       }
+   }
+   
+   printf("\n determinant gösterimi:\n");
+   
+   for(i=0; i<2; i++){
+       printf("|");
+       for(j=0; j<2; j++){
+           printf("%d \t" ,matrix[i][j]);
+       }
+       printf("|");
+       printf("\n");
+   }
+   
+   det=(matrix[0][0] * matrix[1][1])-(matrix[0][1] * matrix[1][0]); //dikkat sadece 2x2 için geçerli
+   printf("matrix determinanti=%d" ,det);
+
+    return 0;
+}
