030084a2cfb1b13dd281f4c6e8011979d9d4c3bd
56mery <cetinkol9226@gmail.com>
2023-12-18 00:44:54 +0300

    c diziler

---
+++ c programing/dizi.c
@@ -0,0 +1,33 @@
+//BİSMİLLAH
+
+int main() {
+   int x=0,max=0,min=0,toplam=0;
+   int num[4];
+   
+   for(x=0; x<4; x++){
+       printf("%d.nci elemani girmniz:" ,x+1);
+       scanf("%d" ,&num[x]);
+   }
+   
+   for(x=0; x<4; x++){
+       printf("num[%d]= %d \n" ,x,num[x]);
+       toplam+=num[x];
+   }
+   
+   min=num[0];
+   max=num[0];
+   
+   for(x=0; x<4; x++){
+       if(num[x]>max){
+           max=num[x];
+       }
+       if(num[x]<min)
+       min=num[x];
+   }
+   
+   printf("en büyük eleman %d ve en küçük eleman %d\n" ,max,min);
+   printf("dizinin elemanları ortalaması %d",toplam/4 );
+  
+
+    return 0;
+}
