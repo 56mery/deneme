fac927515b9defa6a68d5ef666b00e9c742f0729
56mery <cetinkol9226@gmail.com>
2023-12-18 21:06:34 +0300

    c dizilerr

--- c programing/dizi.c
+++ c programing/dizi.c
@@ -70,3 +70,101 @@ int main() {
 
     return 0;
 }
+
+
+//transpoz bulma
+
+int main() {
+   int A[3][3], B[3][3];
+   int row,col,sem;
+   
+   printf("3x3 lük matrisin elemanlaririni giriniz:\n");
+   for(row=0; row<3; row++){
+       for(col=0; col<3; col++){
+           scanf("%d" ,&A[row][col]);
+       }
+   }
+   
+   for(row=0; row<3; row++){
+       for(col=0; col<3; col++){
+           printf("A[%d][%d]=%d \t" ,row,col,A[row][col]);
+       }
+       printf("\n");
+   }
+   
+   for(row=0; row<3; row++){
+       for(col=0; col<3; col++){
+           B[row][col]=A[col][row];
+           printf("A nın transpozu:%d \t" ,B[row][col]);
+       }
+       printf("\n");
+   }
+
+    return 0;
+}
+
+
+//matris oluşturma ve elemanların işaret durumu
+int main()
+{
+   int A[3][3], B[3][3];
+   int row,col,sem,toplam=0;
+   
+   printf("3x3 lük matrisin elemanlaririni giriniz:\n");
+   for(row=0; row<3; row++){
+       for(col=0; col<3; col++){
+           scanf("%d" ,&A[row][col]);
+       }
+   }
+   
+   for(row=0; row<3; row++){
+       for(col=0; col<3; col++){
+           printf("A[%d][%d]=%d \t" ,row,col,A[row][col]);
+       }
+       printf("\n");
+   }
+   
+   for(row=0; row<3; row++){
+       for(col=0; col<3; col++){
+           if(A[row][col]<0){
+               printf("negatif elemanlar A[%d][%d]=%d \n" ,row,col,A[row][col]);
+           }
+           else{
+               printf("pozitif elemanlar A[%d][%d]=%d\n" ,row,col,A[row][col]);
+           }
+           toplam+=A[row][col];
+       }
+   }
+   printf("elemanlar toplami:%d" ,toplam);
+
+
+    return 0;
+}
+
+
+//strinkler
+int main()
+{
+   char nick[10];
+   int d;
+   printf("dizinin uzunlugunu giriniz:");
+   scanf("%d" ,&d);
+   while(1){
+       if(d>10){
+       printf("dizi için boyut çok uzun. lütfen tekrar bir boyut giriniz:");
+       scanf("%d" ,&d);
+       }
+       else break;
+       
+   }
+   
+ printf("bir nick giriniz:");
+ scanf("%s" ,nick);
+ printf("hi,%s" ,nick);
+   
+   
+
+    return 0;
+}
+
+//düzeltme 
