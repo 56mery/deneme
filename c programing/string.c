56022e6ea4e0f5176f2a3f361f1ba10834c7fd21
56mery <cetinkol9226@gmail.com>
2023-12-18 23:16:14 +0300

    c strings

--- c programing/string.c
+++ c programing/string.c
@@ -23,3 +23,82 @@ int main()
 
     return 0;
 }
+
+
+/* toggle case
+#include <stdio.h>
+#include <string.h>
+*/
+
+void bkharf(char * nick){
+    int i=0;
+    while(nick[i] != '\0'){
+        if(nick[i]>='a' && nick[i]<='z'){
+            nick[i]=nick[i]-32;
+        }
+        else if(nick[i]>='A' && nick[i]<='Z'){
+            nick[i]=nick[i]+32;
+        }
+        i++;
+    }
+}
+
+int main()
+{
+  char nick[20] = "JeonJungkook";
+  char nick1[20] = "JeonJess";
+  
+  bkharf (nick);
+  printf("%s" ,nick);
+  
+    return 0;
+}
+
+
+//harf ya da sayı buldurma
+void bkharf(char * nick){
+    int i=0;
+    while(nick[i] != '\0'){
+        if(nick[i]>='a' && nick[i]<='z'){
+           printf("%c in alphabet \n" ,nick[i]);
+        }
+        else if(nick[i]>='A' && nick[i]<='Z'){
+           printf("%c in alphabet\n" ,nick[i]);
+        }
+        else{
+            printf("%c a number\n" ,nick[i]);
+        }
+        i++;
+    }
+}
+
+int main()
+{
+  char nick[20] = "JeonJungkook97";
+  char nick1[20] = "JeonJess";
+  
+  bkharf (nick);
+  printf("%s" ,nick);
+  
+    return 0;
+}
+
+//ters cevirme
+int main()
+{
+  char nick[20] = "JeonJungkook97";
+  char nick1[20] = "JeonJess";
+  
+  int i=0;
+  
+  for(i=0; i<15; i++){
+      for(int j=15; j>0; j--){
+          nick[i]=nick[j];
+          printf("%c" ,nick[i]);
+      }
+  }
+  
+   
+    return 0;
+}
+
