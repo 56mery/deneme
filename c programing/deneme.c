9a9d6d617b0f1990dd048058148a665030f0f256
56mery <cetinkol9226@gmail.com>
2023-12-24 00:08:43 +0300

    c strings

---
+++ c programing/deneme.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(){
+     
+    int toplam=0, sayi1, sayi2, max, min;
+    
+    printf("bir sayı giriniz:");
+    scanf("%d" ,&sayi1);
+    
+    printf("ikinci sayıyı giriniz:");
+    scanf("%d" ,&sayi2);
+    
+    if(sayi2>sayi1){
+        max=sayi2;
+        min=sayi1;}
+    
+    else{
+    max=sayi1;
+    min=sayi2;}
+    
+   
+    
+    while(min<max){
+       toplam+=min;
+       min++;
+   }
+    toplam-=min;
+    
+    printf("%d ve %d arasındaki sayıların toplamı:%d\n" ,min ,max, toplam);
+    return 0;
+ 
+ }
