8bccc68344110891390625742f4c2a41e7b4792e
56mery <cetinkol9226@gmail.com>
2023-12-26 00:12:35 +0300

    c pointers

--- c programing/pointers.c
+++ c programing/pointers.c
@@ -1,3 +1,21 @@
+//easy
+int main() {
+  int *p;
+  int i=30;
+  
+  p=&i;
+
+  
+  printf("pointerin gösterdiği değer %d\n" ,*p);
+  printf("pointerin gösterdiği adres %x\n" ,p);
+  printf("değiskenin adresi %x\n" ,&i);
+  printf("değiskenin değeri %d" ,i);
+
+    return 0;
+}
+
+
+
 //girilen değerleri topluyor
 int main() {
   int x[6],sum=0;
@@ -58,5 +76,76 @@ int main() {
   printf("sayi1= %d adresi %x\n" ,num1,&num1);
   printf("sayi2= %d adresi %x" ,num2,&num2);
   
+    return 0;
+}
+
+  
+//
+int main() {
+  int *pa, *pi;
+  int i,a;
+  int toplam=0;
+ 
+  printf("birinci sayi:");
+  scanf("%d" ,&i);
+  printf("ikinci sayi:");
+  scanf("%d" ,&a);
+  
+  pa=&a;
+  pi=&i;
+  
+  toplam=*(pi)+ *(pa);
+  printf("iki değer toplami %d" ,toplam);
+
+    return 0;
+}
+
+//
+int main() {
+  int *pa, *pi;
+  int i,a;
+  int toplam=0;
+ 
+  printf("birinci sayi:");
+  scanf("%d" ,&i);
+  printf("ikinci sayi:");
+  scanf("%d" ,&a);
+  
+  pa=&a;
+  pi=&i;
+  
+  if((*pa)<(*pi)){
+      printf("ikinci pointerin gösterdiği değer daha küçük");
+  }
+  
+  else if((*pa)>(*pi))
+{
+    printf("ilk pointerin gösterdiği değer daha küçük");
+}
+else printf("iki pointer aynı değerleri gösteriyor");
+
+    return 0;
+}
+
+
+//////////
+
+int main() {
+  int arr[5];
+  int *p;
+  int i;
+  
+  for(i=0; i<5; i++){
+  printf("arr[%d] giriniz:" ,i+1);
+  scanf("%d" ,&arr[i]);
+  }
+  
+  p=&arr[0];
+  
+  for(i=0; i<5; i++){
+      printf("arr[%d]= %d\n" ,i,*(p+i));
+  }
+  
+    
     return 0;
 }
--- c programing/string.c
+++ c programing/string.c
@@ -120,3 +120,177 @@ int main() {
     
     return 0;
 }
+
+//strig uzunluğu #include <stdio.h>
+
+int main() {
+    char text[100];
+    int i,count=0;
+    
+    printf("bir string giriniz:");
+    gets(text);
+    
+    for(i=0; text[i]!='\0'; i++){
+        count++;
+    }
+    
+    printf("stringin uzunluğu: %d" ,count);
+    
+    return 0;
+}
+
+//string uzunluğu space dahil edilmeyen en doğru hali(sonradan aklıma geldi)
+//#include <stdio.h>
+#define MAX_SİZE 100
+
+int main() {
+    char metin[MAX_SİZE];
+    int i,sayac=0;
+    
+    printf("bir string giriniz:");
+    gets(metin);
+    
+    for(i=0; metin[i] !='\0'; i++){
+        if(metin[i]==32){ //32 space in ascıı karşılığı
+            sayac-=1;
+        }
+        sayac+=1;
+    }
+    printf("kelime sayisi %d" ,sayac);
+    
+    return 0;
+}
+
+//iki stringi birleştirir
+#define MAX_SİZE 100
+
+int main() {
+    char metin[MAX_SİZE] ,metin2[MAX_SİZE] ;
+    int i,j;
+    
+    printf("bir string giriniz:");
+    gets(metin);
+    printf("bir string daha giriniz:");
+    gets(metin2);
+    
+    i=0;
+    while(metin[i] != '\0'){
+        i++;
+    }
+    
+    j=0;
+    while(metin2[j] != '\0'){
+        metin[i]=metin2[j];
+        i++;
+        j++;
+    }
+    metin[i]='\0';
+
+    printf("yeni metin %s" ,metin);
+
+    return 0;
+}
+
+//iki stringi karşılaştırır
+#define MAX_SİZE 100
+
+char compare(char *x, char *y){
+    int i;
+  
+       while(x[i]==y[i]){
+           if(x[i]=='\0' && y[i]=='\0')
+           break;
+           i++;
+       }
+   
+   return x[i]-y[i];
+}
+
+int main() {
+    char metin[MAX_SİZE] ,metin2[MAX_SİZE] ;
+    int i,j,ss;
+    
+    printf("bir string giriniz:");
+    gets(metin);
+    printf("bir string daha giriniz:");
+    gets(metin2);
+    
+    ss=compare(metin,metin2);
+    if(ss==0){
+        printf("cümleler/kelimeler aynidir");
+    }
+    
+    else if(ss<0){
+        printf("birinci k/c daha kisa");
+    }
+    else{
+        printf("ikinci cümle daha uzun");
+    }
+
+    return 0;
+}
+
+//büyük harfleri küçük küçük harfleri büyük yapan program
+#define MAX_SİZE 100
+
+void toggleCase(char *x){
+    int i=0;
+  
+  while(x[i]!='\0')
+{
+    if(x[i]>='a' && x[i]<='z'){
+        x[i]-=32;
+    }
+    else if(x[i]>='A' && x[i]<='Z'){
+        x[i]+=32;
+    }
+    i++;
+}  
+}
+
+int main() {
+    char metin[MAX_SİZE] ;
+
+    printf("bir string giriniz:");
+    gets(metin);
+    
+    toggleCase(metin);
+    
+    printf("yeni metin %s" ,metin);
+    
+
+    return 0;
+}
+
+//cümlenin içeriğini bulma
+#define MAX_SİZE 100
+
+int main() {
+    char metin[MAX_SİZE] ;
+     int i=0,harf=0,sayi=0,other=0;
+
+    printf("bir string giriniz:");
+    gets(metin);
+    
+     while(metin[i]!='\0')
+    {
+    if((metin[i]>='a' && metin[i]<='z') || (metin[i]>='A' && metin[i]<='Z')){
+      harf++;
+    }
+    else if(metin[i]>='0' && metin[i]<='9'){
+        sayi++;
+    }
+    else other++;
+    
+    i++;
+    }  
+  
+    
+    printf(" metin %s\n" ,metin);
+     printf("harf sayisi %d\n" ,harf);
+      printf("sayi sayisi %d\n" ,sayi);
+       printf("others sayisi %d" ,other);
+    
+
+    return 0;
+}
