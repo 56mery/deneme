a710fabd2d6ea50642356d80c49387f3e2bbbca1
56mery <cetinkol9226@gmail.com>
2024-01-12 23:35:22 +0300

    dizi.c

--- c programing/dizi.c
+++ c programing/dizi.c
@@ -142,7 +142,7 @@ int main()
 }
 
 
-//strinkler
+//stringler
 int main()
 {
    char nick[10];
@@ -168,3 +168,26 @@ int main()
 }
 
 //düzeltme 
+
+//Kullanıcıdan dizi uzunlugunu (n) alınız. N adet sayı alarak diziye ekleyiniz. Diziyi ters sırada yazdırınız.
+int main() {
+    int n,i;
+    
+    printf("dizinin uzunlugunu girnizn:");
+    scanf("%d" ,&n);
+       int dizi[5]; //burada 5 yerine n var c compiler da çalışıyor ama burda hata veriyor ondan 5 yazdım
+    
+    for(i=0; i<n; i++){
+        printf("%d. elemani giriniz:" ,i+1);
+        scanf("%d" ,&dizi[i]);
+        // toplam+=dizi[i]; topalmını da diğer soruda istemiş tekrar yazmaya gerek yok buraya ekledim sadece ustte toplam declare etmek lazım int toplam=0;
+    }
+    
+     //printf("%d toplam" ,toplam);
+
+    for(i=n-1; i>=0; i--){
+        printf("%d" ,dizi[i]);
+    }
+    
+    return 0;
+} 
